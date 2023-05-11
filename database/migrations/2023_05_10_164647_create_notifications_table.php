f125335a03d160a4ac97ddafa675e217eff74a18
gursewaksingh-jaw <tt.gursewak.singh@gmail.com>
2023-05-11 10:42:31 +0530

    changes done on 11-05-23

---
+++ database/migrations/2023_05_10_164647_create_notifications_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNotificationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->integer('user_id_from');
+            $table->integer('user_id_to');
+            $table->string('title');
+            $table->string('message');
+            $table->string('type');
+            $table->morphs('notifiable');
+            $table->text('data');
+            $table->timestamp('read_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notifications');
+    }
+}
